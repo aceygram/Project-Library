b0dbaee480bdb00a733d57073e84d92b74b1ca9c
Promise Okeke <ace4facebook@gmail.com>
2023-04-11 15:32:19 +0100

    did a little finishing touches

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 const myLibrary = [];
 
-function Book(title, author, year, pages, read, notRead) {
+function Book(title, author, year, pages, read) {
   this.title = title;
   this.author = author;
   this.year = year;
   this.pages = pages;
   this.read = read;
-  this.notRead = notRead;
-  //   this.info = () => {
-  //     console.log(`${title} by ${author} in the year ${year}, ${pages} pages, ${read}.`);
-  //   };
 }
 
 function addBookToLibrary() {
@@ -18,143 +14,185 @@ function addBookToLibrary() {
     document.getElementById('addBookModal').className = 'form-hide';
   };
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  toggleRead();
 
-    const bookTitle = document.getElementById('title');
-    const bookAuthor = document.getElementById('author');
-    const bookYear = document.getElementById('year');
-    const bookPages = document.getElementById('pages');
-    const bookRead = document.getElementById('read');
-    const bookNotRead = document.getElementById('not-read');
-
-    if (bookRead.checked === true && bookNotRead.checked === true) {
-      throw new Error('Please mark only one checkbox either Yes or No');
-    } else if (bookRead.checked === true) {
-      bookRead.value = true;
-      bookNotRead.value = false;
-      // return document.getElementById("result").innerHTML = y;
-    } else if (bookNotRead.checked === true) {
-      bookRead.value = false;
-      bookNotRead.value = true;
-      // return document.getElementById("result").innerHTML = n;
-    } else {
-      throw new Error('*Please mark any of checkbox');
-    }
-
-    const newBook = new Book(
-      bookTitle.value,
-      bookAuthor.value,
-      bookYear.value,
-      bookPages.value,
-      bookRead.value,
-      bookNotRead.value,
-    );
-
-    // if (bookRead.value === true)
-
-    myLibrary.push(newBook);
-
-    console.log(myLibrary);
-
-    const books = document.getElementById('books');
-    const div = document.createElement('div');
-    div.className = 'test';
-    books.appendChild(div);
-
-    if (myLibrary.length < 4) {
-      books.className = 'autofill';
-    } else {
-      books.className = 'autofit';
-    }
-
-    const div1 = document.createElement('div');
-    div1.className = 'title';
-
-    //  });
-    //   for (let i = 0; i < myLibrary.length; i++) {
-    //   if (i === myLibrary.length) {
-    //     return `${myLibrary[i].title}`
-    //   }
-    //   div1.textContent = `${myLibrary[i].title}`;
-    // }
-
-    // for title
-    myLibrary.forEach((object, index) => {
-      if (index === myLibrary.length) {
-        return `${myLibrary[index].title}`;
-      }
-
-      const title = `${myLibrary[index].title}`;
-      const words = title.split(' ');
-      const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
-      const capitalizedTitle = capitalizedWords.join(' ');
-
-      div1.textContent = capitalizedTitle;
-      return true;
-    });
-    div.appendChild(div1);
-
-    // for author
-    const div2 = document.createElement('div');
-    div2.className = 'author';
-
-    myLibrary.forEach((object, index) => {
-      if (index === myLibrary.length) {
-        return `${myLibrary[index].author}`;
-      }
-
-      const author = `${myLibrary[index].author}`;
-      const words = author.split(' ');
-      const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
-      const capitalizedAuthor = capitalizedWords.join(' ');
-
-      div2.textContent = capitalizedAuthor;
-      return true;
-    });
-    div.appendChild(div2);
-
-    // for year
-    const div3 = document.createElement('div');
-    div3.className = 'year';
-    myLibrary.forEach((object, index) => {
-      if (index === myLibrary.length) {
-        return `${myLibrary[index].year}`;
-      }
-
-      div3.textContent = `${myLibrary[index].year}`;
-      return true;
-    });
-
-    div.appendChild(div3);
-
-    // for pages
-    const div4 = document.createElement('div');
-    div4.className = 'pages';
-    myLibrary.forEach((object, index) => {
-      if (index === myLibrary.length) {
-        return `${myLibrary[index].pages}`;
-      }
-
-      div4.textContent = `${myLibrary[index].pages} Pages`;
-      return true;
-    });
-    div.appendChild(div4);
-
-    bookTitle.value = null;
-    bookAuthor.value = null;
-    bookYear.value = null;
-    bookPages.value = null;
-    bookRead.value = ' ';
-    bookNotRead.value = ' ';
-    bookRead.checked = false;
-    bookNotRead.checked = false;
-
-    document.getElementById('addBookModal').classList = 'form-hide';
+  // Update class of books element based on length of myLibrary
+  const books = document.getElementById('books');
+  const directChildren = books.children.length;
+  if (directChildren >= 3) {
+    books.className = 'autofit';
+  } else {
+    books.className = 'autofill';
   }
 
   const form = document.querySelector('form');
   form.addEventListener('submit', handleSubmit);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const bookTitle = document.getElementById('title');
+  const bookAuthor = document.getElementById('author');
+  const bookYear = document.getElementById('year');
+  const bookPages = document.getElementById('pages');
+  const bookRead = document.getElementById('read');
+
+  if (bookRead.checked === true) {
+    bookRead.value = 'Read';
+  } else if (bookRead.checked === false) {
+    bookRead.value = 'Not Read';
+  }
+
+  const newBook = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookYear.value,
+    bookPages.value,
+    bookRead.value,
+  );
+
+  myLibrary.push(newBook);
+
+  const books = document.getElementById('books');
+  const directChildren = books.children.length;
+
+  const div = document.createElement('div');
+  div.className = 'test';
+  books.appendChild(div);
+
+  if (directChildren >= 3) {
+    books.className = 'autofit';
+  } else {
+    books.className = 'autofill';
+  }
+
+  const div1 = document.createElement('div');
+  div1.className = 'title';
+
+  // for title
+  const title = `${newBook.title}`;
+  const words = title.split(' ');
+  const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  const capitalizedTitle = capitalizedWords.join(' ');
+
+  div1.textContent = capitalizedTitle;
+
+  div.appendChild(div1);
+
+  // for author
+  const div2 = document.createElement('div');
+  div2.className = 'author';
+
+  const author = `${newBook.author}`;
+  const words1 = author.split(' ');
+  const capitalizedWords2 = words1.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+  const capitalizedAuthor = capitalizedWords2.join(' ');
+
+  div2.textContent = capitalizedAuthor;
+  div.appendChild(div2);
+
+  // for year
+  const div3 = document.createElement('div');
+  div3.className = 'year';
+
+  div3.textContent = `${newBook.year}`;
+
+  div.appendChild(div3);
+
+  // for pages
+  const div4 = document.createElement('div');
+  div4.className = 'pages';
+  div4.textContent = `${newBook.pages} Pages`;
+
+  div.appendChild(div4);
+
+  // for card buttons
+  const cardButtons = document.createElement('div');
+  cardButtons.className = 'card-buttons';
+  div.appendChild(cardButtons);
+
+  // for read or not read
+  const readButton = document.createElement('button');
+  readButton.className = 'read';
+
+  if (newBook.read === 'Read') {
+    readButton.textContent = 'Read';
+  } else {
+    readButton.textContent = 'Not Read';
+  }
+
+  readButton.textContent = `${newBook.read}`;
+
+  cardButtons.appendChild(readButton);
+
+  // for the svg
+  const svg = document.createElement('svg');
+
+  svg.addEventListener('click', () => {
+    const bookIndex = Array.from(books.children).indexOf(div);
+    removeBook(bookIndex);
+  });
+
+  cardButtons.appendChild(svg);
+
+  toggleRead();
+
+  bookTitle.value = null;
+  bookAuthor.value = null;
+  bookYear.value = null;
+  bookPages.value = null;
+  bookRead.value = ' ';
+  bookRead.checked = false;
+
+  document.getElementById('addBookModal').classList = 'form-hide';
+}
+
+function toggleRead() {
+  document.querySelectorAll('.read').forEach((button) => {
+    button.addEventListener('click', () => {
+      // toggle the text between "Read" and "Not Read"
+      if (button.textContent === 'Read') {
+        button.textContent = 'Not Read';
+      } else {
+        button.textContent = 'Read';
+      }
+    });
+  });
+}
+
+function removeBook(index) {
+  document.getElementById('deleteBookModal').className = 'confirm';
+  const yes = document.getElementById('yes');
+  const no = document.getElementById('no');
+  const books = document.getElementById('books');
+  const directChildren = books.children.length;
+
+  const clickedDiv = books.children[index];
+
+  yes.addEventListener('click', () => {
+    // remove the book from the array
+    myLibrary.splice(index, 1);
+
+    // remove the book from the DOM
+    books.removeChild(clickedDiv);
+
+    document.getElementById('deleteBookModal').classList = 'form-hide';
+
+    // update the autofill/autofit class on the books element
+    if (directChildren >= 3) {
+      books.className = 'autofit';
+    } else {
+      books.className = 'autofill';
+    }
+  });
+
+  no.addEventListener('click', () => {
+    // books.appendChild(div);
+
+    // remove the book from the DOM
+    document.getElementById('deleteBookModal').classList = 'form-hide';
+  });
 }
 
 //   return bookTitle;
